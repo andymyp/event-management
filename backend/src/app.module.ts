943d182eb3e8de72692d3a3dc9493a57bcf9614a
M. Yudistiandy Prabowo <andymyp1997@gmail.com>
2024-12-06 14:53:56 +0700

    BE: prisma service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
 import { LoggerModule } from './logger/logger.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule, LoggerModule],
+  imports: [ConfigModule, LoggerModule, PrismaModule],
 })
 export class AppModule {}
