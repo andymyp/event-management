e4ead16b7911fa77ad2a103130198b34c155399e
M. Yudistiandy Prabowo <andymyp1997@gmail.com>
2024-12-06 14:37:16 +0700

    BE: logger module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, LoggerModule],
 })
 export class AppModule {}
